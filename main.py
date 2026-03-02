3627f08c707e9f9e549ba99cc619722425375c2b
Miles <78830914+MortifiedMiles@users.noreply.github.com>
2026-03-01 20:06:25 -0600

    Co-authored-by: Rob <Robulus@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class Simulator():
         self._mm_size = self.bigOlInput("memory")
         self._pg_size = self.bigOlInput("page")
         self._cache_size = self.bigOlInput("cache")
+        #self._mapping = self.pick_config("mapping")
+        #self._replacement = self.pick_policy("replacement")
 
         while True:
             try:
@@ -64,10 +66,64 @@ class Simulator():
         print(self._cache)
 
     
-    def pick_config(self):
+    def pick_config(self, config):
+        while True:
+            try:
+                inpt = input(f"Pick a {config} config: \nConfig 1, Direct Mapping. \nConfig 2, Fully Associative. \nConfig 3, k-way Associatvie Mapping") 
+                print(inpt)
+                print(type(inpt))
+                if inpt not in [1, 2, 3]:
+                    print("Pick a policy #")
+                    continue
+                if inpt == 2:
+                    self._k = 1
+                elif inpt == 3:
+                    try:
+                        self._k = input(f"Pick a k-value")
+                        if type(self._k) is not int:
+                            print("Not a valid k-value")
+                            continue
+                        break
+                    except  ValueError:
+                        print("BAD")
+                break
+            except ValueError:
+                print("BAD")
+        return inpt
+
+    def pick_policy(self, policy):
+        while True:
+            try:
+                inpt = input(f"Pick a {policy} policy: \nPolicy 1, Replacement policy cache collisions are resolved by overwriting.\n Policy 2, prioritize empty slots.\n Policy 3, prioritize the least recently used")
+                if inpt not in [1, 2, 3]:
+                    print("Pick a policy #")
+                    continue
+                break
+            except ValueError:
+                print("BAD")
+        return inpt
+    
+    def full_replacement(self, address):
         pass
 
-    def generate(self):
+    def k_way_associative(self, ):
+        pass
+                
+    def kway(self):
+        for i in range(self._sample):
+            num = format(int(rand.randint(0,self._mm_size)),'09b')
+
+            _set_size = self._cache_size / 4
+
+            k_len = len(format(int(4),'09b'))
+            k_tag = self.addr_len - self.offset - k_len
+            _set = int(num[k_tag:k_tag+k_len])
+
+            
+
+
+
+    def direct(self):
 
         for i in range(self._sample):
             print("------")
@@ -92,7 +148,6 @@ class Simulator():
                 print(f"cached tag: {self._cache[_line][0:self.tag]}")
                 print(f"   new tag: {num[0:self.tag]}")
                 self._cache
-            
 
 
             # print(num)
@@ -111,8 +166,10 @@ class Simulator():
 
 
 def test():
+    
     test = Simulator()
     test.preprocessing()
-    test.generate()   
+    #test.pick_policy()
+    test.kway()   
 
 test()
